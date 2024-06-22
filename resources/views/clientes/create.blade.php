7ff0c0095a01d9d32a3f3686abffbc342bd1215e
Paul <paulrodrigoguasace@gmail.com>
2024-06-21 23:16:00 -1100

    vista create cliente en 2 columnas

--- resources/views/clientes/create.blade.php
+++ resources/views/clientes/create.blade.php
@@ -11,45 +11,49 @@
         <div class="card-body">
             <form action="{{ route('clientes.store') }}" method="POST">
                 @csrf
-
-                <div class="form-group">
-                    <label for="nombreCompleto">Nombre Completo</label>
-                    <input type="text" name="nombreCompleto" id="nombreCompleto" class="form-control" required>
+                <div class="row">
+                    <div class="col-md-6">
+                        <div class="form-group">
+                            <label for="nombreCompleto">Nombre Completo</label>
+                            <input type="text" name="nombreCompleto" id="nombreCompleto" class="form-control" required>
+                        </div>
+        
+                        <div class="form-group">
+                            <label for="telefono">Teléfono</label>
+                            <input type="text" name="telefono" id="telefono" class="form-control" required>
+                        </div>
+        
+                        <div class="form-group">
+                            <label for="email">Email</label>
+                            <input type="email" name="email" id="email" class="form-control" required>
+                        </div>
+                        <div class="form-group">
+                            <label for="direccion">Dirección</label>
+                            <input type="text" name="direccion" id="direccion" class="form-control" required>
+                        </div>
+                    </div>
+                    
+                    <div class="col-md-6">
+                        
+                        <div class="form-group">
+                            <label for="genero">Género</label>
+                            <select name="genero" id="genero" class="form-control" required>
+                                <option value="Masculino">Masculino</option>
+                                <option value="Femenino">Femenino</option>
+                            </select>
+                        </div>
+        
+                        <div class="form-group">
+                            <label for="fechaNacimiento">Fecha de Nacimiento</label>
+                            <input type="date" name="fechaNacimiento" id="fechaNacimiento" class="form-control">
+                        </div>
+        
+                        <div class="form-group">
+                            <label for="formaPago">Forma de Pago</label>
+                            <input type="text" name="formaPago" id="formaPago" class="form-control">
+                        </div>
+                    </div>
                 </div>
-
-                <div class="form-group">
-                    <label for="telefono">Teléfono</label>
-                    <input type="text" name="telefono" id="telefono" class="form-control" required>
-                </div>
-
-                <div class="form-group">
-                    <label for="email">Email</label>
-                    <input type="email" name="email" id="email" class="form-control" required>
-                </div>
-
-                <div class="form-group">
-                    <label for="direccion">Dirección</label>
-                    <input type="text" name="direccion" id="direccion" class="form-control" required>
-                </div>
-
-                <div class="form-group">
-                    <label for="genero">Género</label>
-                    <select name="genero" id="genero" class="form-control" required>
-                        <option value="Masculino">Masculino</option>
-                        <option value="Femenino">Femenino</option>
-                    </select>
-                </div>
-
-                <div class="form-group">
-                    <label for="fechaNacimiento">Fecha de Nacimiento</label>
-                    <input type="date" name="fechaNacimiento" id="fechaNacimiento" class="form-control">
-                </div>
-
-                <div class="form-group">
-                    <label for="formaPago">Forma de Pago</label>
-                    <input type="text" name="formaPago" id="formaPago" class="form-control">
-                </div>
-
                 <button type="submit" class="btn btn-primary">Guardar Cliente</button>
                 <a href="{{ route('clientes.index') }}" class="btn btn-secondary">Cancelar</a>
             </form>
