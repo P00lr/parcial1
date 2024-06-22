779128d4f6f67f5feed0ecbce59f015cb932ea53
ferpazj <fernandoyourmen@hotmail.com>
2024-06-22 01:40:33 -0400

    edit,show.blade actualizada

--- resources/views/producto/edit.blade.php
+++ resources/views/producto/edit.blade.php
@@ -12,38 +12,44 @@
             <form action="{{ route('productos.update', $Productos->id) }}" method="POST">
                 @csrf
                 @method('PUT')
-                <div class="form-group">
-                    <label for="nombre">Nombre</label>
-                    <input type="text" name="nombre" id="nombre" class="form-control" value="{{ $Productos->nombre }}" required>
-                </div>
-                <div class="form-group">
-                    <label for="cantidad">Cantidad</label>
-                    <input type="number" name="cantidad" id="cantidad" class="form-control" value="{{ $Productos->cantidad }}" required>
-                </div>
-                <div class="form-group">
-                    <label for="precio">Precio</label>
-                    <input type="number" name="precio" id="precio" class="form-control" value="{{ $Productos->precio }}" required>
-                </div>
-                <div class="form-group">
-                    <label for="descripcion">Descripcion</label>
-                    <input type="text" name="descripcion" id="descripcion" class="form-control" value="{{ $Productos->descripcion }}" required>
-                </div>
-                <div class="form-group">
-                    <label for="marca">Marca</label>
-                    <input type="text" name="marca" id="marca" class="form-control" value="{{ $Productos->marca }}" required>
-                </div>
-                <div class="form-group">
-                    <label for="agregado_fecha">Fecha de ingreso</label>
-                    <input type="date" name="agregado_fecha" id="agregado_fecha" class="form-control" value="{{ $Productos->agregado_fecha}}" required>
-                </div>
-                <div class="form-group">
-                    <label for="fecha_vencimiento">Fecha de vencimiento</label>
-                    <input type="date" name="fecha_vencimiento" id="fecha_vencimiento" class="form-control" value="{{ $Productos->fecha_vencimiento }}" required>
-                </div>
-                <div class="form-group">
-                    <label for="categoria">Categoria</label>
-                    <input type="text" name="categoria" id="categoria" class="form-control" value="{{ $Productos->categoria}}" required>
-                </div>
+              <div class="row">
+                    <div class="col-md-6">
+                      <div class="form-group">
+                        <label for="nombre">Nombre</label>
+                        <input type="text" name="nombre" id="nombre" class="form-control" value="{{ $Productos->nombre }}" required>
+                      </div>
+                      <div class="form-group">
+                        <label for="cantidad">Cantidad</label>
+                        <input type="number" name="cantidad" id="cantidad" class="form-control" value="{{ $Productos->cantidad }}" required>
+                      </div>
+                      <div class="form-group">
+                        <label for="precio">Precio</label>
+                        <input type="number" name="precio" id="precio" class="form-control" value="{{ $Productos->precio }}" required>
+                      </div>
+                      <div class="form-group">
+                        <label for="descripcion">Descripcion</label>
+                        <input type="text" name="descripcion" id="descripcion" class="form-control" value="{{ $Productos->descripcion }}" required>
+                      </div>
+                   </div>
+                    <div class="col-md-6">
+                      <div class="form-group">
+                        <label for="marca">Marca</label>
+                        <input type="text" name="marca" id="marca" class="form-control" value="{{ $Productos->marca }}" required>
+                      </div>
+                     <div class="form-group">
+                        <label for="agregado_fecha">Fecha de ingreso</label>
+                        <input type="date" name="agregado_fecha" id="agregado_fecha" class="form-control" value="{{ $Productos->agregado_fecha}}" required>
+                      </div>
+                     <div class="form-group">
+                        <label for="fecha_vencimiento">Fecha de vencimiento</label>
+                        <input type="date" name="fecha_vencimiento" id="fecha_vencimiento" class="form-control" value="{{ $Productos->fecha_vencimiento }}" required>
+                     </div>
+                     <div class="form-group">
+                        <label for="categoria">Categoria</label>
+                        <input type="text" name="categoria" id="categoria" class="form-control" value="{{ $Productos->categoria}}" required>
+                      </div>
+                    </div>
+             </div>
                 <button type="submit" class="btn btn-primary">Guardar cambios</button>
             </form>
         </div>
